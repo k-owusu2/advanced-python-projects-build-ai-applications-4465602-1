b1b10e48e54dde95a6f575be7d81df5fbcfb0f1a
Kofi Owusu <owus2812@stthomas.edu>
2025-08-26 18:51:09 -0500

    Refactor Car class documentation and improve comments for clarity

--- Begin/CH_0_OOP_Refresher.py
+++ Begin/CH_0_OOP_Refresher.py
@@ -1,20 +1,28 @@
 # Introduction to Object-Oriented Programming with Python: Creating and Using Classes
 
 # Class Definition
+class Car:
 
 # Constructor (Initialization) - __init__ method
+  def __init__(self, make, model):
+    
 
 # Encapsulation: Attributes (make and model) are encapsulated within the class.
-
+    self.make = make  # Attribute: make of the car
+    self.model = model  # Attribute: model of the car
 
 # Method - start_engine
-
-# Encapsulation: Accessing attributes through self.
+  def start_engine(self):
+    print(f"The engine of the {self.make} {self.model} is running.")
 
 
 # Creating instances (objects) of the Car class
 
 # Inheritance: Car is a class that can be used to create objects (instances).
+car1 = Car("Acura", "TLX")
+car2 = Car("Honda", "Accord")
+print(f'I have a {car1.make} {car1.model}.')
+print(f'I have a {car2.make} {car2.model}.')
 # Abstraction: We create objects without worrying about the internal details of the Car class.
 
 # Creating the first car (object)
@@ -26,10 +34,8 @@
 
 # Encapsulation: Accessing object attributes (make and model) using dot notation.
 
-
 # Calling object methods
-
 # Polymorphism: Different objects (car1 and car2) can perform the same action (start_engine).
-
-
 # Method Call - start_engine
+car1.start_engine() 
+car2.start_engine()
